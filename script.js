a541163a9ad3abb056a7f96548e8ed2e32dd25c0
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-05-07 20:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
-    //Write your code here
 
+    var firstName = document.getElementById("firstName").value;
+    
+    var lastName = document.getElementById("lastName").value;
+
+
+    var fullName;
+    if (lastName.includes(" ")) {
+        fullName = firstName + " " + lastName;
+    } else {
+        fullName = firstName + " " + lastName;
+    }
+
+    alert(fullName);
 }
